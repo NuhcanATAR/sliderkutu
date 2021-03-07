e7dbde8ea8f78b61240d13adda8279c722a467d9
Nuhcan <nuhcanatar20@gmail.com>
2021-03-07 07:53:14 +0300

    Add files via upload

---
+++ suggested_interactions_page.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <meta name="description"
+      content="Sende Yeni Ber Deneyim İçin Hesap Oluştur">
+      <meta name="author" content="TurkuazTech">
+
+      <meta property="og:site_name" content="TurkuazTech"/>
+      <meta property="og:site" content="http://www.turkuaztech.com/"/>
+      <meta property="og:title" content=""/>
+      <meta property="og:description" content="Sende Yeni Ber Deneyim İçin Hesap Oluştur"/>
+      <meta property="og:image" content=""/>
+      <meta property="og:url" content="http://www.turkuaztech.com/"/>
+      <meta property="og:type" content="article"/>
+
+      <title>Social Media | Önerilen Etkileşimler</title>
+
+      <link rel="icon" href="img/favicon.png" type="image/png" sizes="16x16">
+
+      <link rel="stylesheet" href="css/suggested_interactions_page.css">
+
+
+      <script src=”https://code.jquery.com/jquery-3.4.1.min.js“></script>
+
+      <!--
+      fontawesom
+      -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/brands.min.css" integrity="sha512-AMDXrE+qaoUHsd0DXQJr5dL4m5xmpkGLxXZQik2TvR2VCVKT/XRPQ4e/LTnwl84mCv+eFm92UG6ErWDtGM/Q5Q==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/fontawesome.min.css" integrity="sha512-8jdwayz5n8F2cnW26l9vpV6+yGOcRAqz6HTu+DQ3FtVIAts2gTdlFZOGpYhvBMXkWEgxPN3Y22UWyZXuDowNLA==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/regular.min.css" integrity="sha512-qgtcTTDJDk6Fs9eNeN1tnuQwYjhnrJ8wdTVxJpUTkeafKKP6vprqRx5Sj/rB7Q57hoYDbZdtHR4krNZ/11zONg==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/solid.min.css" integrity="sha512-QN7X/bUbbeel9bbq6JVNJXk1Zowt+n+QPN+DjhEmTa4TdL1YPCsCey5JrvfRW8xp28LDYgGG/waNVdrhwMQmVQ==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/svg-with-js.min.css" integrity="sha512-gX1L9ovb6aHWz/bJYE364vKdGBDOeZLMt168kVRpRdo2I2TpveBJpbDzHMZQWy8liTTG7Qm9Onl1Ji92Eo3x7Q==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/v4-shims.min.css" integrity="sha512-njWvXjJPMYfvYHb2DAnEL0MS5KEmoIeZVdpK7tpxiApoL3ZoUzHSvkoSFrbASdTuIHnEVG+Ko+ES/DXKygBH4g==" crossorigin="anonymous" />
+
+  </head>
+ <body>
+
+
+ <!-- Head Text Content Start -->
+  <div class="body_head_text">
+     <h1 class="animation a1">Nuhcan, Sana Önerebilceğimiz Bir Kaç Kullanıcımızı Göstereceğiz:)</h1>
+  </div>
+ <!-- Head Text Content End -->
+
+<style>
+.mySlides{display: none;}
+</style>
+
+<!-- Suggested scrolling start -->
+  <div class="suggested_scroll_content">
+
+ <button class="btns_left"  onclick="plusDivs(-1)"><i class="fas fa-chevron-circle-left"></i></button>
+
+
+<!-- Suggested slider content start -->
+  <div class="mySlides">
+    <!-- Suggested content start -->
+      <div class="suggested_content">
+         <center>
+           <div class="header_content">
+              <img src="img/pp.jpg" alt="">
+           </div>
+         </center>
+         <div class="header_body_content">
+            <h1>
+              NuhcanATAR
+              <i class="fas fa-check-circle"></i>
+            </h1>
+         </div>
+         <div class="header_footer_content">
+            <h4>@nuhcanatar</h4>
+         </div>
+         <div class="body_head_content">
+            <p>8.4M Etkileşim</p>
+         </div>
+         <div class="body_body_content">
+            <p>Photo Chat CEO</p>
+         </div>
+          <center>
+         <div class="body_footer_content">
+              <a href="#demo-modal">
+               <button type="submit" name="button" class="tick">
+                 <i class="fas fa-user-check"></i>
+                 Etkileşim İsteği Gönder
+               </button>
+              </a>
+              <div id="demo-modal" class="modal">
+              <div class="modal__content">
+                <img src="img/checked.png" alt="">
+                <h3>Tebrikler !</h3>
+                <h3>Etkileşim İsteği Gönderildi</h3>
+              <div class="modal__footer">
+              </div>
+              <a href="#" class="modal__close">&times;</a>
+              </div>
+              </div>
+
+         </div>
+         </center>
+      </div>
+    <!-- Suggested content end -->
+
+  </div>
+  <!-- Suggested slider content end -->
+
+
+  <!-- Suggested slider content start -->
+    <div class="mySlides">
+      <!-- Suggested content start -->
+        <div class="suggested_content">
+           <center>
+             <div class="header_content">
+                <img src="img/human_1.jpg" alt="">
+             </div>
+           </center>
+           <div class="header_body_content">
+              <h1>
+                Bekircan GÖZAY
+                <i class="fas fa-check-circle"></i>
+              </h1>
+           </div>
+           <div class="header_footer_content">
+              <h4>@bekircangzy</h4>
+           </div>
+           <div class="body_head_content">
+              <p>10.4M Etkileşim</p>
+           </div>
+           <div class="body_body_content">
+              <p>Photo Chat CEO</p>
+           </div>
+            <center>
+           <div class="body_footer_content">
+                <a href="#demo-modall">
+                 <button type="submit" name="button"  class="tick">
+                   <i class="fas fa-user-check"></i>
+                   Etkileşim İsteği Gönder
+                 </button>
+                </a>
+                <div id="demo-modall" class="modal">
+                <div class="modal__content">
+                  <img src="img/checked.png" alt="">
+                  <h3>Tebrikler !</h3>
+                  <h3>Etkileşim İsteği Gönderildi</h3>
+                <div class="modal__footer">
+                </div>
+                <a href="#" class="modal__close">&times;</a>
+                </div>
+                </div>
+
+           </div>
+           </center>
+        </div>
+      <!-- Suggested content end -->
+
+    </div>
+    <!-- Suggested slider content end -->
+
+
+ <button class="btns_right"  onclick="plusDivs(1)"><i class="fas fa-chevron-circle-right"></i></button>
+
+  </div>
+ <!-- suggested scrolling end -->
+
+ <script>
+ var slideIndex = 1;
+ showDivs(slideIndex);
+
+ function plusDivs(n) {
+   showDivs(slideIndex += n);
+ }
+
+ function showDivs(n) {
+   var i;
+   var x = document.getElementsByClassName("mySlides");
+   if (n > x.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = x.length}
+   for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+   }
+   x[slideIndex-1].style.display = "block";
+ }
+
+
+
+
+
+
+ </script>
+ </body>
+</html>
